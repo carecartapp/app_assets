573dc26e2c0826c8a628a183cf62568b1428748f
aleemahmadvaival <39512784+aleemahmadvaival@users.noreply.github.com>
2019-02-15 14:21:06 +0500

    Update js-storefront-script.js

--- js-storefront-script.js
+++ js-storefront-script.js
@@ -821,6 +821,16 @@ function AbandonedCart() {
     function closeDiscountSpinnerPopup() {
         carecartJquery('#cc-spinner-body').remove();
     }
+    
+    function getParameterByName(name, url) {
+if (!url) url = window.location.href;
+name = name.replace(/[\[\]]/g, "\\$&");
+var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+results = regex.exec(url);
+if (!results) return null;
+if (!results[2]) return '';
+return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
 
     function addJqueryEventListeners() {
