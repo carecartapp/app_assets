52f874d29464fb4e52d10bb43d4173265f0e159a
aleemahmadvaival <39512784+aleemahmadvaival@users.noreply.github.com>
2019-07-22 16:26:13 +0500

    Update js-storefront-script.js

--- js-storefront-script.js
+++ js-storefront-script.js
@@ -1,8 +1,10 @@
 // js-storefront-script.js GH v.1.2.2
-// Updated at: 24-04-2019
+// Updated at: 22-07-2019
 var isAjax = 0;
 var isCartLoading = 0;
 var isCheckForCall = true;
+var isCheckForMobile = false;
+
 function getQueryParameters() {
     var prmstr = window.location.search.substr(1);
     return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
@@ -53,6 +55,13 @@ function AbandonedCart() {
 
     this.init = function (callback, callbackArgs) {
         console.log("Initialization started");
+
+
+
+       if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
+            isCheckForMobile = true;
+        }
 
         scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
             scriptInjection("https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/md5.js", function () {
@@ -734,26 +743,63 @@ function showProPnSubscriptionPopup(popupData) {
                         isImpressionCapturedByPushNotification = true;
                         setTimeout(function () {
                             carecartJquery('body').append(preparedHtml);
-                            if (popupData.appearance_location == 'TOP_CENTER') {
-                                carecartJquery('#cc_pn_notification_template').css({
-                                    'top': '5px',
-                                    'left': '37%'
-                                });
-                            } else if (popupData.appearance_location == 'TOP_LEFT') {
-                                carecartJquery('#cc_pn_notification_template').css({
-                                    'top': '5px',
-                                    'left': '5px'
-                                });
-                            } else if (popupData.appearance_location == 'TOP_RIGHT') {
-                                carecartJquery('#cc_pn_notification_template').css({
-                                    'top': '5px',
-                                    'right': '15px'
-                                });
-                            } else if (popupData.appearance_location == 'BOTTOM_RIGHT') {
-                                carecartJquery('#cc_pn_notification_template').css({
-                                    'bottom': '0px',
-                                    'right': '15px'
-                                });
+                            carecartJquery('#cc_pn_notification_template').css({'position': 'fixed'});
+                                if(window.innerWidth >= 1012) {
+                                    var left = '35%';
+                                    var width = '420px';
+                                }else{
+                                    var left = '25%';
+                                    var width = '50%';
+                                }
+
+                                if (popupData.appearance_location == 'TOP_CENTER') {
+                                    carecartJquery('#cc_pn_notification_template').css({
+                                        'top': '5px',
+                                        'left': left,
+                                        'width': width
+                                    });
+                                } else if (popupData.appearance_location == 'TOP_LEFT') {
+                                    carecartJquery('#cc_pn_notification_template').css({
+                                        'top': '5px',
+                                        'left': '5px'
+                                    });
+                                } else if (popupData.appearance_location == 'TOP_RIGHT') {
+                                    carecartJquery('#cc_pn_notification_template').css({
+                                        'top': '5px',
+                                        'right': '15px'
+                                    });
+                                } else if (popupData.appearance_location == 'BOTTOM_RIGHT') {
+                                    carecartJquery('#cc_pn_notification_template').css({
+                                        'bottom': '0px',
+                                        'right': '15px'
+                                    });
+                                }
+                            if(isCheckForMobile){
+                                if(window.innerWidth <= 768 && window.innerWidth >= 525) {
+                                var left = '20%';
+                                var width = '65%';
+                                    if (popupData.appearance_location == 'TOP_CENTER') {
+                                        carecartJquery('#cc_pn_notification_template').css({
+                                        'left': left,
+                                        'width': width
+                                        });
+                                    }
+
+                                }else if(window.innerWidth <= 525){
+                                    var left = '-8px';
+                                    var right = '2%';
+                                    var width = '95%';
+                                    carecartJquery('#cc_pn_notification_template').css({
+                                    'left': left,
+                                    'right': right,
+                                    'width': width
+                                    });
+                                        carecartJquery('#pn-optin-disallow-btn-text').css({
+                                            'width': 'auto'
+                                        });
+
+
+                                }
                             }
                         }, popupData.popup_delay_in_seconds + '000');
 
