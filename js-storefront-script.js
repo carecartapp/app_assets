e4f465886b4ee497a32ef434c2ed7703b02b48fa
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2020-04-03 16:44:15 +0500

    Updates: Spinner conflict removed
    Updates: Spinner conflict removed

--- js-storefront-script.js
+++ js-storefront-script.js
@@ -1,6 +1,6 @@
 // js-storefront-script GH v.1.5.0
 // Updated at: 04-03-2020
-// https://cdn.jsdelivr.net/gh/carecartapp/app_assets@1.5.0/
+// https://cdn.jsdelivr.net/gh/carecartapp/app_assets@1.5.1/
 var isAjax = 0;
 var isCartLoading = 0;
 var isCheckForCall = true;
@@ -51,7 +51,7 @@ function AbandonedCart() {
     var isSupportOfWholeSale =0;
     var store = {};
     var apiBaseUrl = "https://app-er.carecart.io";
-    var scriptBuildUrl = 'https://cdn.jsdelivr.net/gh/carecartapp/app_assets@1.5.0/';
+    var scriptBuildUrl = 'https://cdn.jsdelivr.net/gh/carecartapp/app_assets@1.5.1/';
     var ccPnAuthUrl = "pn-app-er.carecart.io";
     var pnSubscriptionPopupData = {};
     var pnChildWindowData = {};
