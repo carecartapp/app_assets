9cdf117b65053c916af3e392d9c13720e9d79bf9
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2020-04-27 15:34:16 +0500

    Updates Spinner cache implemented
    Updates Spinner cache implemented

--- js-storefront-script.js
+++ js-storefront-script.js
@@ -1,6 +1,6 @@
-// js-storefront-script GH v.1.5.1
-// Updated at: 04-03-2020
-// https://cdn.jsdelivr.net/gh/carecartapp/app_assets@1.5.1/
+// js-storefront-script GH v.1.5.2
+// Updated at: 27-04-2020
+// https://cdn.jsdelivr.net/gh/carecartapp/app_assets@1.5.2/
 var isAjax = 0;
 var isCartLoading = 0;
 var isCheckForCall = true;
@@ -51,7 +51,7 @@ function AbandonedCart() {
     var isSupportOfWholeSale =0;
     var store = {};
     var apiBaseUrl = "https://app-er.carecart.io";
-    var scriptBuildUrl = 'https://cdn.jsdelivr.net/gh/carecartapp/app_assets@1.5.1/';
+    var scriptBuildUrl = 'https://cdn.jsdelivr.net/gh/carecartapp/app_assets@1.5.2/';
     var ccPnAuthUrl = "pn-app-er.carecart.io";
     var pnSubscriptionPopupData = {};
     var pnChildWindowData = {};
