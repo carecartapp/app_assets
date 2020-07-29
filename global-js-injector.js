8924640977d2ac1eeefc9500385c2a1500d1b4e2
CareCart Shopify APP <44872750+carecartapp@users.noreply.github.com>
2020-07-29 23:41:06 +0500

    Spinner tracking server updated
    Spinner tracking server updated

--- global-js-injector.js
+++ global-js-injector.js
@@ -1,6 +1,6 @@
-// global-js-injector GH v.1.6.0
+// global-js-injector GH v.1.6.1
 // Updated at: 29-07-2020
-// https://cdn.jsdelivr.net/gh/carecartapp/app_assets@1.6.0/
+// https://cdn.jsdelivr.net/gh/carecartapp/app_assets@1.6.1/
 
 function getQueryParameters() {
     var prmstr = window.location.search.substr(1);
@@ -41,7 +41,7 @@ function AbandonedCart() {
 
     var store = {};
     var apiBaseUrl = "https://app-er.carecart.io";
-    var scriptBuildUrl = 'https://cdn.jsdelivr.net/gh/carecartapp/app_assets@1.6.0/';
+    var scriptBuildUrl = 'https://cdn.jsdelivr.net/gh/carecartapp/app_assets@1.6.1/';
     this.init = function (callback, callbackArgs) {
         console.log("Initialization started");
         scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
